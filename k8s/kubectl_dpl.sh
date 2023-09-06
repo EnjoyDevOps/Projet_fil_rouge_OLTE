b12187ebf2d3a4a607227a13c6f8f54263662cb9
OLTE <oliver.hyperion@gmail.com>
2023-09-06 15:27:07 +0200

    Projet final

---
+++ k8s/kubectl_dpl.sh
@@ -0,0 +1,7 @@
+kubectl apply -f client_node.yml
+kubectl apply -f comments_node.yml
+kubectl apply -f query_node.yml
+kubectl apply -f posts_node.yml
+kubectl apply -f event_bus_node.yml
+kubectl apply -f moderation_node.yml
+kubectl apply -f ingress.yml
