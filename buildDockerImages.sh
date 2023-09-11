5e4beef81d4dc2ed3c973d97b0d941625ca3b839
OLTE <oliver.hyperion@gmail.com>
2023-09-11 02:40:19 +0200

    renommage fichier

---
+++ buildDockerImages.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+# Changer le shebang! selon votre shell
+# 
+# Assurez-vous que le fichier est executable sous votre shell
+# Executer : chmod 700 BuildDockerImages.sh
+# Puis : ./BuildDockerImages
+#
+# Script de construction des images docker "react-directory"
+directories=("client" "comments" "event-bus" "moderation" "posts" "query")
+
+for directory in "${directories[@]}"; do
+    docker build -t "react-$directory" "./$directory"
+done
