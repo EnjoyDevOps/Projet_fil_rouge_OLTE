f62fdc714380060a96954435b11921e8e3139a32
OLTE <oliver.hyperion@gmail.com>
2023-09-11 03:24:08 +0200

    maj commentaires

--- kubeDplServices.sh
+++ kubeDplServices.sh
@@ -2,8 +2,8 @@
 # Changer le shebang! selon votre shell
 # 
 # Assurez-vous que le fichier est executable sous votre shell
-# Executer : chmod 700 KubeDpl.sh
-# Puis : ./KubeDpl
+# Executer : chmod 700 KubeDplServices.sh
+# Puis : ./KubeDplServices
 #
 # Script de deployement des services Kubernetes
 kubeServices=("client" "comments" "event-bus" "moderation" "posts" "query" "ingress")
